d79d47fef406aa9d9db86c3cb6a01c0bd78a3ca6
Opilane TTHK <opilane@tthk.ee>
2022-11-21 11:26:31 +0200

    context_progress
    context_progress

---
+++ ShopTARgv21.Data/ShopDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShopTARgv21.Core.Domain;
+
+namespace ShopTARgv21.Data
+{
+    public class ShopDbContext : DbContext
+    {
+        public ShopDbContext(DbContextOptions<ShopDbContext> options)
+            : base(options) { }
+
+        public DbSet<Spaceship> Spaceship { get; set; }
+            )
+
+    }
+}
