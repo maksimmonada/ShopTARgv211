fa1dd7d8e78e71fc914d603502b41a75088ea2ab
Opilane TTHK <opilane@tthk.ee>
2022-11-28 11:35:02 +0200

    Spacelistmodel controll
    Spacelistmodel controll

--- Shop/Controllers/SpaceshipController.cs
+++ Shop/Controllers/SpaceshipController.cs
@@ -1,10 +1,33 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Shop.Models.Spaceship;
+using ShopTARgv21.Data;
 
 namespace Shop.Controllers
 {
     public class SpaceshipController : Controller
     {
+        private readonly ShopDbContext _context;
+
+        public SpaceshipController
+            (
+            ShopDbContext context
+            )
+        {
+            _context = context;
+        }
         public IActionResult Index()
+        {
+            var result = _contextSpaceship
+                .OrderByDescending(y => y.CreatedAt)
+                .Select(XmlConfigurationExtensions => new SpaceshipListViewModel
+                {
+                    Name = x.Name;
+            ModelType = x.ModelType;
+            Passenger = x.Passenger;
+            BuidOfDate = x.BuidOfDate;
+            LaunchDate = x.LaunchDate
+                })
+
         {
             return View();
         }
---
+++ Shop/Models/Spaceship/SpaceshipListViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Shop.Models.Spaceship
+{
+    public class SpaceshipListViewModel : Controller
+    {
+        public string Name { get; set; }
+        public string ModelType { get; set; }
+        public string SpaceshipBuider { get; set; }
+        public string PlaceOfBuild { get; set; }
+        public int EnginePower { get; set; }
+        public int LiftUpToSpaceByTonn { get; set; }
+        public int Crew { get; set; }
+        public string Passengers { get; set; }
+        public DateTime LaunchDate { get; set; }
+        public DateTime BuildOfDate { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime ModifiedAt { get; set; }
+    }
+}
